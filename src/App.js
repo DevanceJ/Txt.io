bdad817837bc73bfef477b0672700b1fda06bc66
Devance Joshi <dj788@snu.edu.in>
2023-02-07 21:46:30 +0530

    added textarea and features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './componenets/Navbar';
+import Textarea from './componenets/Textarea';
 
 function App() {
   return (
     <>
-  <Navbar/>
+  <Navbar title = "txt.io"/>
+  <div className="container">
+  <Textarea/>
+  </div>
     </>
   );
 }
---
+++ src/componenets/Textarea.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+export default function Textarea() {
+    const downloadFile = () => {
+        let content = text
+        const element = document.createElement("a");
+        const blob = new Blob([content], { type: "plain/text" });
+        const fileUrl = URL.createObjectURL(blob);
+        let filename = "mytext"
+        element.setAttribute("href", fileUrl); //file location
+        element.setAttribute("download", filename); // file name
+        element.style.display = "none";
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+      };
+    const [text, setText] = useState("");
+    const toUp = ()=>{
+        let upTEXT = text.toUpperCase();
+        setText(upTEXT);
+    }
+    const toLow = ()=>{
+        let upTEXT = text.toLowerCase();
+        setText(upTEXT);
+    }
+    const copy = ()=>{
+        navigator.clipboard.writeText(text)
+    }
+    const changeUp = (e)=>{
+        setText(e.target.value)
+    }
+    return (
+    <>
+    <div>
+        <h1 className='my3'>
+            Enter text
+        </h1>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={changeUp} id="exampleFormControlTextarea1" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={toUp}>To uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={toLow}>To lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={copy}>Copy to clipboard</button>
+        <button className="btn btn-primary mx-1" onClick={downloadFile}>Download</button>
+    </div>
+    <div className="container my-3">
+        <p>{text.split(" ").length-1} words and {text.length} characters</p>
+    </div>
+    </>)
+}
